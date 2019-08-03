ec4424fd3cf5280290413492c852bde77360e430
Duy Le Ngoc <duy.le@pycogroup.com>
2019-08-04 02:31:36 +0700

    chore(structure): initial default structure of xcode

---
+++ ShowCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowCase
+//
+//  Created by Duy Le Ngoc on 8/3/19.
+//  Copyright © 2019 PYCO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShowCaseTests/ShowCaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  ShowCaseTests.swift
+//  ShowCaseTests
+//
+//  Created by Duy Le Ngoc on 8/3/19.
+//  Copyright © 2019 PYCO. All rights reserved.
+//
+
+import XCTest
+@testable import ShowCase
+
+class ShowCaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
